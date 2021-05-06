8b6a775e30652d66eba69311bb1bf7dc196789d1
learnwithaman <learnwithaman@gmail.com>
2021-05-06 18:27:14 +0530

    Add ghi, gti, and pg to chart

--- src/components/BiaxialLineChart.js
+++ src/components/BiaxialLineChart.js
@@ -13,44 +13,44 @@ import {
 const data = [
   {
     name: 'Page A',
-    uv: 4000,
-    pv: 2400,
+    ghi: 4000,
+    gti: 2400,
     amt: 2400,
   },
   {
     name: 'Page B',
-    uv: 3000,
-    pv: 1398,
+    ghi: 3000,
+    gti: 1398,
     amt: 2210,
   },
   {
     name: 'Page C',
-    uv: 2000,
-    pv: 9800,
+    ghi: 2000,
+    gti: 9800,
     amt: 2290,
   },
   {
     name: 'Page D',
-    uv: 2780,
-    pv: 3908,
+    ghi: 2780,
+    gti: 3908,
     amt: 2000,
   },
   {
     name: 'Page E',
-    uv: 1890,
-    pv: 4800,
+    ghi: 1890,
+    gti: 4800,
     amt: 2181,
   },
   {
     name: 'Page F',
-    uv: 2390,
-    pv: 3800,
+    ghi: 2390,
+    gti: 3800,
     amt: 2500,
   },
   {
     name: 'Page G',
-    uv: 3490,
-    pv: 4300,
+    ghi: 3490,
+    gti: 4300,
     amt: 2100,
   },
 ];
@@ -75,13 +75,28 @@ function BiaxialLineChart({ width, height }) {
       <Tooltip />
       <Legend />
       <Line
+        name='GHI'
         yAxisId='left'
         type='monotone'
-        dataKey='pv'
+        dataKey='gti'
         stroke='#8884d8'
         activeDot={{ r: 8 }}
       />
-      <Line yAxisId='right' type='monotone' dataKey='uv' stroke='#82ca9d' />
+      <Line
+        name='GTI'
+        yAxisId='left'
+        type='monotone'
+        dataKey='amt'
+        stroke='#ffc107'
+        activeDot={{ r: 8 }}
+      />
+      <Line
+        name='PG'
+        yAxisId='right'
+        type='monotone'
+        dataKey='ghi'
+        stroke='#82ca9d'
+      />
     </LineChart>
   );
 }
